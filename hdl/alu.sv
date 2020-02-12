eece32bebb936e7cb3d8315cd28dce3bcd6a5cf9
cs141 <jswingos@college.harvard.edu>
2020-02-11 19:17:32 -0800

    initial commit

---
+++ hdl/alu.sv
@@ -0,0 +1,11 @@
+module alu
+    (
+        input logic [31:0] x,
+        input logic [31:0] y,
+        input logic [2:0] op,
+        output logic [31:0] z,
+        output logic zero, equal, overflow
+    );
+
+    // your code here
+endmodule
